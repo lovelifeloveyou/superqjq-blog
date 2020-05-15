d9075508b18601db33bb669097a302ee419faaff
lovelifeloveyou <1902996802@qq.com>
2020-05-15 17:10:22 +0800

    55

--- theme-vdoing/components/NavLinks.vue
+++ theme-vdoing/components/NavLinks.vue
@@ -91,7 +91,7 @@ export default {
       if (repo) {
         return /^https?:/.test(repo)
           ? repo
-          : `https://github.com/${repo}`
+          : `https://lovelifeloveyou.github.io/${repo}`
       }
       return null
     },
